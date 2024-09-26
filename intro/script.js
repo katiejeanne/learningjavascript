4b319dc2da33214338a8b4d8401792c92e808f71
Katie Wessman <kjeanne@gmail.com>
2024-09-26 14:49:58 -0500

    Completes exercise on assigning variables

---
+++ intro/script.js
@@ -0,0 +1,17 @@
+const country = 'Potato Land';
+const continent = 'Atlantis';
+let population = 5;
+const isIsland = true;
+const language = 'Potatoish';
+
+language = 'Taroish';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
--- valuesandvariables/script.js
+++
@@ -1,8 +0,0 @@
-let country = 'Potato Land'
-let continent = 'Atlantis'
-let population = '5'
-
-console.log(country);
-console.log(continent);
-console.log(population);
-
