f11b893768d9dd2876c5645d39163e84e5a313dc
Katie Wessman <kjeanne@gmail.com>
2024-09-27 15:26:04 -0500

    Completes operators exercise

--- intro/script.js
+++ intro/script.js
@@ -4,14 +4,35 @@ let population = 5;
 const isIsland = true;
 const language = 'Potatoish';
 
-language = 'Taroish';
+// language = 'Taroish';
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+console.log(population / 2);
+
+population++;
 console.log(population);
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+
+
+const finlandPop = 6;
+console.log(population > finlandPop);
+
+const avgPopulation = 33;
+console.log(population < avgPopulation);
+
+console.log(country +
+    ' is in ' +
+    continent +
+    ' and its ' +
+    population +
+    ' people speak ' +
+    language
+)
 
 
