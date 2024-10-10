828a8a08783873611a5ee456cb0b62be8f8242d1
Katie Wessman <kjeanne@gmail.com>
2024-10-10 13:29:14 -0500

    Adds scoping challenge from section 8.

---
+++ challenges/section8_scoping/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      // New variable with same name as outer variable.
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      // Reassigning variable created in parent
+      output = 'NEW OUTPUT!';
+    }
+
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
