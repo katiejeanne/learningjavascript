f4e878b674650e9ecc22de93d017cb8345c013c1
Katie Wessman <kjeanne@gmail.com>
2024-11-18 19:31:47 -0600

    FInishes Challenge 3 for Section 16

--- lecture_notes/section16_async/script.js
+++ lecture_notes/section16_async/script.js
@@ -117,10 +117,10 @@ const renderCountry = function (data, className = '') {
 // );
 // console.log(request);
 
-// const renderError = function (msg) {
-//   countriesContainer.insertAdjacentText('beforeend', msg);
-//   //   countriesContainer.style.opacity = 1;
-// };
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //   countriesContainer.style.opacity = 1;
+};
 
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then((response) => {
@@ -206,34 +206,125 @@ const getCountryData = function (country) {
 // The executor function takes two arguments - resolve and reject which are functions
 //
 
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   (position) => resolve(position),
+//     //   (err) => reject(err)
+//     // );  Same as below
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// getPosition().then((pos) => console.log(pos));
+
+// const whereAmI = function (lat, long) {
+//   getPosition()
+//     .then((pos) => {
+//       const { latitude: lat, longitude: long } = pos.coords;
+//       return fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
+//     })
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+//       getCountryData(data.country);
+//     })
+//     .catch((error) => console.log(error));
+// };
+
+// btn.addEventListener('click', whereAmI);
+
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
-    // navigator.geolocation.getCurrentPosition(
-    //   (position) => resolve(position),
-    //   (err) => reject(err)
-    // );  Same as below
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-getPosition().then((pos) => console.log(pos));
+const whereAmI = async function () {
+  try {
+    const position = await getPosition();
+    const { latitude: lat, longitude: long } = position.coords;
 
-const whereAmI = function (lat, long) {
-  getPosition()
-    .then((pos) => {
-      const { latitude: lat, longitude: long } = pos.coords;
-      return fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error(`Problem with geocoding ${res.status}`);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.country}`);
-      getCountryData(data.country);
-    })
-    .catch((error) => console.log(error));
+    const countryData = await fetch(
+      `https://geocode.xyz/${lat},${long}?geoit=json`
+    );
+    if (!countryData.ok) throw new Error('Problem getting location data.');
+    const dataGeo = await countryData.json();
+
+    const response = await fetch(
+      `https://countries-api-836d.onrender.com/countries/name/${dataGeo.country}`
+    );
+    const data = await response.json();
+    renderCountry(data[0]);
+
+    // return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    return dataGeo.city;
+  } catch (err) {
+    console.log(err);
+    renderError(`Ooogly boooogly`);
+
+    // Reject promise returned from async function
+    throw err;
+  }
 };
 
-btn.addEventListener('click', whereAmI);
+// console.log('1: Will get location');
+// // const city = whereAmI();
+// // console.log(city);
+
+// whereAmI()
+//   .then((city) => console.log(city))
+//   .catch((err) => console.error())
+//   .finally(() => console.log('3: Finished getting location'));
+// // console.log('3: Finished getting location');
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(city);
+  } catch (err) {
+    console.log(`2: ${err.message}`);
+  } finally {
+    console.log('3: finished getting location');
+  }
+});
+
+//Async await is syntactic sugar over promises &  then statements
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   x = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    //     const [data1] = await getJSON(
+    //       `https://countries-api-836d.onrender.com/countries/name/${c1}`
+    //     );
+    //     const [data2] = await getJSON(
+    //       `https://countries-api-836d.onrender.com/countries/name/${c2}`
+    //     );
+    //     const [data3] = await getJSON(
+    //       `https://countries-api-836d.onrender.com/countries/name/${c3}`
+    //     );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`),
+    ]);
+    console.log(data.map((d) => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
